850dbdedf73c38f4512ae7b9bc7755716902b740
rzlco666 <syahrizalhanif@gmail.com>
2021-12-11 18:03:33 +0700

    Install Petugas Template

---
+++ application/controllers/Petugas.php
@@ -0,0 +1,122 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Petugas extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('m_petugas');
+    }
+
+    public function index()
+    {
+
+        /* if ($this->session->userdata('is_login') == FALSE) {
+            redirect('/', 'refresh');
+        } */
+
+        $this->load->view('petugas/layout/header');
+        $this->load->view('petugas/layout/sidebar');
+        $this->load->view('petugas/index');
+        $this->load->view('petugas/layout/footer');
+    }
+
+    public function login()
+    {
+
+        /* if ($this->session->userdata('is_login') == TRUE) {
+            redirect('petugas/index', 'refresh');
+        } */
+        
+
+        $this->load->view('petugas/login/index');
+    }
+
+    public function register()
+    {
+
+       /*  if ($this->session->userdata('is_login') == TRUE) {
+            redirect('petugas/index', 'refresh');
+        } */
+
+        $this->load->view('petugas/register/index');
+    }
+
+    public function register_proses()
+    {
+
+        $this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[3]|max_length[22]');
+        $this->form_validation->set_rules('email', 'E-mail', 'trim|required|min_length[3]|max_length[45]|is_unique[user.email]');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[5]|max_length[12]');
+
+        if ($this->form_validation->run() == TRUE) {
+
+            if ($this->m_petugas->m_register()) {
+
+                $this->session->set_flashdata('pesan', 'Register berhasil, silahkan  Sign In.');
+                redirect('/', 'refresh');
+            } else {
+
+                $this->session->set_flashdata('pesan', 'Register user gagal!');
+                redirect('/', 'refresh');
+            }
+        } else {
+
+            $this->load->view('petugas/register/index');
+        }
+    }
+
+    public function login_proses()
+    {
+
+        $this->form_validation->set_rules('email', 'E-mail', 'trim|required|min_length[3]|max_length[45]');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[5]|max_length[12]');
+
+        if ($this->form_validation->run() == TRUE) {
+
+            if ($this->m_petugas->m_cek_mail()->num_rows() == 1) {
+
+                $db = $this->m_petugas->m_cek_mail()->row();
+                if (hash_verified($this->input->post('password'), $db->password)) {
+
+                    $data_login = array(
+                        'is_login' => TRUE,
+                        'email'  => $db->email,
+                        'username'   => $db->username
+                    );
+
+                    $this->session->set_userdata($data_login);
+                    redirect('petugas/index', 'refresh');
+                } else {
+
+                    $this->session->set_flashdata('pesan', 'Login gagal: password salah!');
+                    redirect('/', 'refresh');
+                }
+            } else { // jika email tidak terdaftar!
+
+                $this->session->set_flashdata('pesan', 'Login gagal: email salah!');
+                redirect('/', 'refresh');
+            }
+        } else {
+
+            $this->load->view('petugas/login');
+        }
+    }
+
+    public function logout()
+    {
+
+        $this->session->unset_userdata('is_login');
+        $this->session->unset_userdata('username');
+        $this->session->unset_userdata('email');
+
+        session_destroy();
+        //$this->session->set_flashdata('pesan', 'Sign Out Berhasil!');
+        redirect('/', 'refresh');
+    }
+}
+
+/* End of file Petugas.php */
+/* Location: ./application/controllers/Petugas.php */
---
+++ application/views/petugas/layout/footer.php
@@ -0,0 +1,36 @@
+<footer class="main-footer">
+                <div class="footer-left">
+                    Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+                </div>
+                <div class="footer-right">
+
+                </div>
+            </footer>
+        </div>
+    </div>
+
+    <!-- General JS Scripts -->
+    <script src="<?=base_url('assets_petugas/'); ?>modules/jquery.min.js"></script>
+    <script src="<?=base_url('assets_petugas/'); ?>modules/popper.js"></script>
+    <script src="<?=base_url('assets_petugas/'); ?>modules/tooltip.js"></script>
+    <script src="<?=base_url('assets_petugas/'); ?>modules/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?=base_url('assets_petugas/'); ?>modules/nicescroll/jquery.nicescroll.min.js"></script>
+    <script src="<?=base_url('assets_petugas/'); ?>modules/moment.min.js"></script>
+    <script src="<?=base_url('assets_petugas/'); ?>js/stisla.js"></script>
+
+    <!-- JS Libraies -->
+    <script src="<?=base_url('assets_petugas/'); ?>modules/jquery.sparkline.min.js"></script>
+    <script src="<?=base_url('assets_petugas/'); ?>modules/chart.min.js"></script>
+    <script src="<?=base_url('assets_petugas/'); ?>modules/owlcarousel2/dist/owl.carousel.min.js"></script>
+    <script src="<?=base_url('assets_petugas/'); ?>modules/summernote/summernote-bs4.js"></script>
+    <script src="<?=base_url('assets_petugas/'); ?>modules/chocolat/dist/js/jquery.chocolat.min.js"></script>
+
+    <!-- Page Specific JS File -->
+    <script src="<?=base_url('assets_petugas/'); ?>js/page/index.js"></script>
+
+    <!-- Template JS File -->
+    <script src="<?=base_url('assets_petugas/'); ?>js/scripts.js"></script>
+    <script src="<?=base_url('assets_petugas/'); ?>js/custom.js"></script>
+</body>
+
+</html>
