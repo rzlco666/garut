2bc81857cf202738d214a1a964c35fb654f6a9b8
rzlco666 <syahrizalhanif@gmail.com>
2022-01-03 20:07:37 +0700

    Add live chat via twak.to

--- application/views/home/footer.php
+++ application/views/home/footer.php
@@ -51,8 +51,21 @@
 
 </div>
 
-
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+  var Tawk_API = Tawk_API || {},
+    Tawk_LoadStart = new Date();
+  (function() {
+    var s1 = document.createElement("script"),
+      s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/61d2e1ebc82c976b71c48723/1fofs50os';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+  })();
+</script>
+<!--End of Tawk.to Script-->
 
 
 <script type='text/javascript' src='<?= base_url('assets_wisatawan/'); ?>js/jquery.js'></script>
